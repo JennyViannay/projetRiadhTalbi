45f3d381a2557d9890430f3c0fa885ba6323a374
Raidtal <raidtal@gmail.com>
2019-01-28 21:52:54 +0100

    csv export

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ProductController extends AbstractController {
 
 
@@ -66,6 +67,77 @@ class ProductController extends AbstractController {
             "product" => $product,
             "Current_menu" => "products"
         ]);
+
+    }
+
+
+    /**
+     * @Route("/export", name="product.export" )
+     * @return Response
+     */
+    public function export()
+    {
+
+        $events  = $this->repository->findAll();
+
+        $rows = array();
+        foreach ($events as $event) {
+            $data = array($event->getId(), $event->getNom(), $event->getDescription(), $event->getPrix());
+
+            $rows[] = implode(',', $data);
+        }
+
+
+        $content = implode("\n", $rows);
+        $response = new Response($content);
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename="produits.csv"');
+        return $response;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*$sql="select id, nom,description,prix from product";
+        $stm = $em->getConnection()->getWrappedConnection()->prepare($sql);
+        //$stm= $em->getConnection()->prepare($sql);
+        $stm->execute();
+
+        $source = new PDOStatementSourceIterator($stm);
+
+        $alphabet="abcdefghijklmnopqrstuvwxyz123456789";
+        $lettre_aleatoire=$alphabet[rand(0,25)];
+
+// Prepare the writer
+        $writer = new CsvWriter('products.csv');
+
+// Export the data
+
+         Handler::create($source, $writer)->export();
+
+        //return $this->redirectToRoute('product.index');
+        $publicResourcesFolderPath = $this->get('kernel')->getRootDir() . '/public/';
+        $filename = "products.csv";
+
+        // This should return the file located in /mySymfonyProject/web/public-resources/TextFile.txt
+        // to being viewed in the Browser
+        return new BinaryFileResponse($publicResourcesFolderPath.$filename);*/
+
+
+
+
+
+
+
+
 
     }
 
